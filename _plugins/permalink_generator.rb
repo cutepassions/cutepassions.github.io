725d5c56da635a651ca084e10b990955d3fe0928
진병욱 <quddnr678@naver.com>
2024-10-14 23:37:48 +0900

    플러그인 수정

--- _plugins/permalink_generator.rb
+++ _plugins/permalink_generator.rb
@@ -5,14 +5,18 @@ module Jekyll
     def generate(site)
       site.pages.each do |page|
         if page.ext == ".md"
-          # 현재 디렉토리의 Markdown 파일 수를 셉니다.
-          md_files_count = Dir.glob(File.join(File.dirname(page.path), "*.md")).size
-
-          # 파일 이름에서 확장자를 제거합니다.
+          # 파일의 상대 경로를 가져옵니다.
+          relative_path = page.path.sub(site.source, "")
+          
+          # 파일의 디렉토리와 이름을 분리합니다.
+          dir = File.dirname(relative_path)
           basename_without_ext = File.basename(page.path, page.ext)
 
           # permalink를 설정합니다.
-          page.data["permalink"] = "/#{md_files_count}/#{basename_without_ext}/"
+          permalink = File.join(dir, basename_without_ext).gsub("\\", "/")
+
+          # 설정된 값을 페이지 데이터에 추가합니다.
+          page.data["permalink"] = permalink
         end
       end
     end
